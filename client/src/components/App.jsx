215ee926f0fdd84ada18d8b954bdef4b99bf004c
Alyssa Wei <wei.alyssa@gmail.com>
2019-06-24 13:51:45 -0700

    Drawing uploaded image to canvas

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,23 +1,54 @@
 import React from 'react';
-
+import path from 'path';
 import axios from 'axios';
+
 
 // import AttendeeForm from './AttendeeForm.jsx';
 // import AttendeeList from './AttendeeList.jsx';
-
+function rect(props) {
+  const { ctx, x, y, width, height } = props;
+  ctx.fillRect(x, y, width, height);
+}
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
       colorpalettes: [],
+      selectedFile: null,
     };
     // this.addAttendee = this.addAttendee.bind(this);
     // this.getAttendees = this.getAttendees.bind(this);
+    this.fileChangedHandler = this.fileChangedHandler.bind(this);
+    this.uploadHandler = this.uploadHandler.bind(this);
   }
 
   componentDidMount() {
-    // this.getAttendees();
+  }
+
+  fileChangedHandler(e) {
+    this.setState({ selectedFile: e.target.files[0] })
+  }
+
+  uploadHandler() {
+    const data = new FormData();
+    data.append("image", this.state.selectedFile, this.state.selectedFile.name);
+
+    return axios.post('http://localhost:3010/' + 'uploadedimage', data)
+      .then(response => {
+        console.log(response)
+        this.updateCanvas(response.data.imageUrl)})
+
+  }
+
+  updateCanvas(url) {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = 'http://localhost:3010/'+ url;
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0, 640, 425);
+      }
   }
 
   // getAttendees() {
@@ -45,11 +76,26 @@ class App extends React.Component {
         </div>
         <div className="leftCol">
           <h2>Color Picker</h2>
-          <h5>Choose 5 colors then click save</h5>
+          <h5>1. Upload an image.</h5>
+          <h5>2. Choose 5 colors then click save.</h5>
+          <input type="file" onChange={this.fileChangedHandler}></input>
+          <button onClick={this.uploadHandler}>Upload!</button>
+          <canvas ref="canvas" width={640} height={425}/>
           {/* < ColorPalettePicker /> */}
         </div>
-      </div>);
+      </div>
+    );
   }
 }
 
 export default App;
+
+
+// TODO render an image
+// TODO make sure upload is saving
+// TODO render uploaded image
+// TODO add color picker
+// TODO add react to color picker
+// TODO save color palette
+// TODO show color palettes in db
+// TODO show color in multiple formats
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const fs = require ('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const multer = require('multer');
+const cors = require('cors');
 const db = require('../db');
 const { getSome, add } = require('./controllers/palette');
 
@@ -10,6 +13,24 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
+module.exports.uploadedFile = path.join(__dirname, './uploaded.jpg');
+
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'client/public/images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+});
+
+const upload = multer({
+  storage
+})
+
+app.use(cors());
+
 
 app.get('/colorpalettes', (req, res) => {
   getSome(req,res);
@@ -17,6 +38,16 @@ app.get('/colorpalettes', (req, res) => {
 
 app.post('/colorpalettes', (req, res) => {
   add(req.body, res);
+});
+
+
+app.post('/uploadedimage', upload.single('image'), (req, res) => {
+  if (req.file)
+    res.json({
+      imageUrl: `images/${req.file.filename}`
+    });
+  else
+    res.status("409").json("No Files to Upload.");
 });
 
 const PORT = process.env.PORT || 3010;
